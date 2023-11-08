f67673345b207ad8d1bece77b51d988d557a48f0
Killian Griset <kgriset@bess-f3r4s9.clusters.42paris.fr>
2023-11-08 11:29:26 +0100

    ft_atoi + main pas ouf

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,13 +6,44 @@
 /*   By: kgriset <kgriset@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 19:21:19 by kgriset           #+#    #+#             */
-/*   Updated: 2023/11/07 22:40:58 by kgriset          ###   ########.fr       */
+/*   Updated: 2023/11/08 11:28:31 by kgriset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <stdio.h> 
 
+int ft_isdigit(int c) {
+  if (c >= '0' && c <= '9')
+    return (c);
+  else
+    return (0);
+}
+
 int ft_atoi(const char *nptr) {
-    
+	int i = 0;
+	int result = 0;
+	int sign = 1;
+	while ( ((nptr[i] >= '\t' && nptr[i] <= '\r') || nptr[i] == ' ') && nptr[i] )
+		i++;
+	if ( nptr[i] == '-' )
+	{
+		sign = -1;		
+		i++;
+	}
+	else if ( nptr[i] == '+' )
+		i++;
+	while ( ft_isdigit((unsigned char)nptr[i]))
+	{
+		result = result * 10 + nptr[i] - '0';
+		i++;
+	}
+	return (result*sign);
+}
+
+int main() { 
+	char *nptr = "   \t\n+42   4";
+	printf("atoi: %i\n", atoi(nptr));
+	printf("ft_atoi: %i\n", ft_atoi(nptr));
+	return (1);
 }

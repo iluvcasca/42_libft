0eeedd4d0a05f56b771199166e72a383d3b7a694
iluvcasca <sntcillian@gmail.com>
2023-11-10 23:35:13 +0000

    chokbar du strlcat

---
+++ ft_strlcat.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kgriset <kgriset@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/10 22:54:03 by kgriset           #+#    #+#             */
+/*   Updated: 2023/11/10 23:29:47 by kgriset          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <bsd/string.h>
+#include <stdio.h>
+
+size_t ft_strlcat(char *dst, const char*src, size_t size)
+{
+    size_t i = 0;
+    size_t j = 0;
+    size_t l_dst = 0;
+    size_t l_src = 0;
+
+    while (dst[i] && i != size)
+        i++;
+    l_dst = i;
+    if (i == size)
+        return (l_dst);
+    else
+    {
+        while (src[j])
+            j++;
+        l_src = j;
+        j = 0;
+        while (size - i - 1> 0 && src[j])
+        {
+            dst[i] = src[j];
+            j++;
+            i++;
+        }
+        dst[i] = '\0';
+        return l_dst + l_src; 
+    }
+}
+
+int main ()
+{
+    char dst[30] = "Hello";
+    char dst1[30] = "Hello";
+    char src[2] = {'4','2'};
+    ft_strlcat( dst, src, 10);
+    printf("dst: %s\n", dst);
+    strlcat(dst1 , src, 10);
+    printf("dst1: %s\n", dst1);
+    return (1);
+}
--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,33 +6,35 @@
 /*   By: kgriset <kgriset@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 10:48:58 by kgriset           #+#    #+#             */
-/*   Updated: 2023/11/10 16:21:19 by kgriset          ###   ########.fr       */
+/*   Updated: 2023/11/10 22:47:29 by kgriset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
 #include <stdio.h>
+#include <bsd/string.h>
 
- size_t ft_strlcpy(char * dst, const char * src, size_t dstsize){
-	 size_t src_len;
-src_len = 0;
-	while(src[src_len])
-		src_len++;
-     while (dstsize > 0)
-     {
-         *dst = *src;
-         dst++;
-         src++;
-         dstsize--;
-     }
-     *dst = '\0';
-     return (src_len);
- }
+size_t ft_strlcpy(char *dst, const char *src, size_t dstsize) {
+  size_t src_len;
+  src_len = 0;
+  while (src[src_len])
+    src_len++;
+  while (dstsize > 1) {
+    *dst = *src;
+    dst++;
+    src++;
+    dstsize--;
+  }
+  *dst = '\0';
+  return (src_len);
+}
 
-int main(){
-    char src[] = "hello"; 
-    char dst[6];
-    printf("size_t: %zu\n", ft_strlcpy (dst, src, 5));
-    printf("dst: %s\n", dst);
-    return (1);
+int main() {
+  char src[] = "hello";
+  char dst[6];
+  printf("size_t: %zu\n", ft_strlcpy(dst, src, 5));
+  printf("dst: %s\n", dst);
+  printf("\nsize_t: %zu\n", strlcpy(dst, src, 5));
+  printf("dst: %s\n", dst);
+
+  return (1);
 }

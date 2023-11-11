6d225633ae087a7145364d6341b7bbb9303364d5
iluvcasca <sntcillian@gmail.com>
2023-11-11 19:35:24 +0000

    working script

--- comment_out.sh
+++ comment_out.sh
@@ -1,6 +1,19 @@
-#! /bin/bash
+#!/bin/bash
 
+# Function to comment out a range of lines in a file
+comment_out_range() {
+    file="$1"
+    sed -i ' /#include <stdlib.h>/! {
+            /#include "libft.h"/! {
+             /#include/,/^}$/ {
+                s/^/\/\//
+            }
+        }
+    }' "$file"
+}
+
+# Iterate through each .c file in the directory
 for file in *.c; do
-    sed -i '/^#include/,/^}/ s/^/\/\//' $file
+    comment_out_range "$file"
 done
-echo "main commented out"
+
--- uncomment.sh
+++ uncomment.sh
@@ -1,6 +1,19 @@
-#! /bin/bash
+#!/bin/bash
 
+# Function to comment out a range of lines in a file
+uncomment_range() {
+    file="$1"
+sed -i ' /#include <stdlib.h>/! {
+            /#include "libft.h"/! {
+             /#include/,/^}$/ {
+                s/^\/\///
+            }
+        }
+    }' "$file"
+}
+
+# Iterate through each .c file in the directory
 for file in *.c; do
-    sed -i '/^\/\/#include/,/^\/\/}/ s/^\/\///' $file
+    uncomment_range "$file"
 done
-echo "main uncommented"
+

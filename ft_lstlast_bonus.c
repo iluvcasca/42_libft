aa320d5abc195298b859b6edce38b1048f97cbf7
iluvcasca <sntcillian@gmail.com>
2023-11-16 17:05:14 +0000

    removed unused value

--- ft_lstlast_bonus.c
+++ ft_lstlast_bonus.c
@@ -6,22 +6,18 @@
 /*   By: kgriset <kgriset@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 12:51:39 by kgriset           #+#    #+#             */
-/*   Updated: 2023/11/16 12:55:58 by kgriset          ###   ########.fr       */
+/*   Updated: 2023/11/16 17:01:35 by kgriset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 t_list *ft_lstlast(t_list *lst)
 {
-    int i;
-
     t_list *last;
 
-    i = 0;
     last = lst;
     while (lst)
     {
-        i++;
         last = lst;
         lst = lst->next;
     }
